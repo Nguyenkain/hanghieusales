c1a39128c8f821978c61b5b2157bf21f9efe6a96
hugnnm <hungnm@newwave.vn>
2015-07-26 01:00:31 +0700

    brands

--- wp-content/themes/hanghieu-mobile/page-thuong-hieu.php
+++ wp-content/themes/hanghieu-mobile/page-thuong-hieu.php
@@ -9,18 +9,23 @@ the_post();
 			<a href="javascript://">Thương hiệu</a>
 	</div>
 </div>
-<div class="brand-section">
-	<div class="container">
-		<?php
-		$args_ = array(
-			'post_type'=>'lshowcase',
-			'showposts'=>-1
-			);
-		$logo = new WP_Query($args_);
-		if($logo->have_posts()):while($logo->have_posts()):$logo->the_post();
-		?>
-		<a href="<?php echo get_post_meta(get_the_ID(),'urllink',true); ?>"><?php the_post_thumbnail('full',array('class'=>'img-responsive logo')); ?></a>
-	<?php endwhile; endif; wp_reset_postdata(); ?>
+<div class="container">
+	<div class="row brands">
+	<?php
+	$args_ = array(
+		'post_type'=>'lshowcase',
+		'showposts'=>-1
+		);
+	$logo = new WP_Query($args_);
+	if($logo->have_posts()):while($logo->have_posts()):$logo->the_post();
+	?>
+		<div class="col-xs-6 col-sm-2 brand-item">
+			<a href="<?php echo get_post_meta(get_the_ID(),'urllink',true); ?>">
+				<img class="img-responsive" src="<?php echo wp_get_attachment_url( get_post_thumbnail_id(get_the_ID()) );?>">
+<!--				--><?php //the_post_thumbnail('post-thumbnail',array('class'=>'img-responsive logo')); ?>
+			</a>
+		</div>
+<?php endwhile; endif; wp_reset_postdata(); ?>
 	</div>
 </div>
 <?php get_template_part('content','footer'); ?>
