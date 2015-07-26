57139c1774b38d7335db639fc3b52048e1abeb17
Nguyenkain <Nguyenkain@gmail.com>
2015-07-27 01:50:14 +0700

    Complete product category page

--- wp-content/themes/hanghieu-mobile/woocommerce/loop/price.php
+++ wp-content/themes/hanghieu-mobile/woocommerce/loop/price.php
@@ -2,8 +2,8 @@
 /**
  * Loop Price
  *
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
+ * @author        WooThemes
+ * @package    WooCommerce/Templates
  * @version     1.6.4
  */
 
@@ -13,10 +13,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $product;
 ?>
-<?php $price = get_post_meta( get_the_ID(), '_regular_price', true);
-$sale = get_post_meta( get_the_ID(), '_sale_price', true); ?>
+<?php $price = get_post_meta( get_the_ID(), '_regular_price', true );
+$sale        = get_post_meta( get_the_ID(), '_sale_price', true ); ?>
 <div class="price-info">
-	<p class="old-price"><?php echo number_format($sale,0,'.','.').' VNĐ</b><br/><span class="old-price">'.number_format($price,0,'.','.') ?> VNĐ</p>
+	<p class="old-price"><?php echo number_format( $sale, 0, '.', '.' ) ?> VNĐ</p>
 
-	<p class="new-price"><?php echo number_format($price,0,'.','.') ?>  VNĐ</p>
+	<p class="new-price"><?php echo number_format( $price, 0, '.', '.' ) ?> VNĐ</p>
 </div>
