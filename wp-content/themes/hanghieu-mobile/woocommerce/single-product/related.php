85bbad51f0fafc79e2c9aca3cbca46ca3a47cd04
Nguyenkain <Nguyenkain@gmail.com>
2015-07-28 01:28:14 +0700

    query post and single product

--- wp-content/themes/hanghieu-mobile/woocommerce/single-product/related.php
+++ wp-content/themes/hanghieu-mobile/woocommerce/single-product/related.php
@@ -71,7 +71,7 @@ if ( $loop->have_posts() ) {
 		<div class="heading-section clearfix">
 			<h2 class="title-section">Sản phẩm bán chạy</h2>
 		</div>
-<div class="row">
+<div class="row sample">
 <?php
 echo '<div class="item-carousel" id="item-carousel">';
 while ( $loop->have_posts() ) : $loop->the_post();
