c19e65c19d5a6cf97619f5af6e5245f60056a3ea
hugnnm <hungnm@newwave.vn>
2015-07-28 01:07:55 +0700

    trang tags

--- wp-content/themes/hanghieu-mobile/config/woocommerce.php
+++ wp-content/themes/hanghieu-mobile/config/woocommerce.php
@@ -81,6 +81,16 @@ function hhs_brand()
       	echo '<a class="hhs_brand" href="'.get_term_link( $pa_brand->term_id,'pa_brand').'" title="'.$pa_brand->name.'">'.$pa_brand->name.'</a>';
         }
 }
+
+function hhs_brand_2()
+{
+	global $product;
+	$pa_brand = get_the_terms( $product->id, 'pa_brand');
+
+	foreach ( $pa_brand as $pa_brand ) {
+		echo $pa_brand->name;
+	}
+}
 // Custom woocommerce_before_shop_loop
 remove_action('woocommerce_before_shop_loop','woocommerce_result_count',20);
 remove_action('woocommerce_before_main_content','woocommerce_breadcrumb',20);
--- wp-content/themes/hanghieu-mobile/woocommerce/archive-product.php
+++ wp-content/themes/hanghieu-mobile/woocommerce/archive-product.php
@@ -1,84 +1,141 @@
 <?php get_header();
 ?>
 <article id="main-content">
-<div class="breacrumb">
-	<div class="container">
-		<a href="#">Trang chủ</a>
+	<div class="breacrumb">
+		<div class="container">
+			<a href="<?php echo home_url(); ?>" title="Trang chủ">Trang chủ</a>
 			<span>//</span>
-			<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
-			<h1 class="page-title"><?php woocommerce_page_title(); ?></h1>
-			<?php endif; ?>
-		/<span><?php echo show_post_count(); ?> <?php _e('sản phẩm','hanghieu'); ?></span>
+			<a href="javascript://">Tags</a>
+		</div>
 	</div>
-</div>
-<div class="section">
 	<div class="container">
-		<div class="row">
-			<?php get_sidebar('product'); ?>
-			<div class="col-md-9">
-				<?php
-					if ( is_product_category() ){
-				    global $wp_query;
-				    $cat = $wp_query->get_queried_object();
-				    $thumbnail_id = get_woocommerce_term_meta( $cat->term_id, 'thumbnail_id', true );
-				    $image = wp_get_attachment_url( $thumbnail_id );
-				    if ( $image ) {
-					    ?>
-					    <div class="featured-image">
-							<img src="<?php echo $image; ?>" alt="<?php woocommerce_page_title(); ?>">
-						</div>
-						<!-- /.featured-image -->
-					    <?php
-					}
-				}
-				?>
-				<?php if ( have_posts() ) : ?>
-				<?php
-				?>
-				<div class="product-grid">
-					<header class="header-grid clearfix">
-						<?php
-							/**
-							* woocommerce_before_shop_loop hook
-							*
-							* @hooked woocommerce_result_count - 20
-							* @hooked woocommerce_catalog_ordering - 30
-							*/
-							do_action( 'woocommerce_before_shop_loop' );
-						?>
-					</header>
-				<?php woocommerce_product_loop_start(); ?>
+		<div class="row tags-page-photo">
+			<img class="img-responsive" src="<?php echo get_template_directory_uri()?>/img/tags.png" alt="header">
+		</div>
 
-				<?php woocommerce_product_subcategories(); ?>
-
-				<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php wc_get_template_part( 'content', 'product' ); ?>
-
-				<?php endwhile; // end of the loop. ?>
-
-				<?php woocommerce_product_loop_end(); ?>
-
-				<?php
-				/**
-				* woocommerce_after_shop_loop hook
-				*
-				* @hooked woocommerce_pagination - 10
-				*/
-				?>
-				</div>
-				<?php
-				do_action( 'woocommerce_after_shop_loop' );
-				?>
-
-				<?php elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) : ?>
-				<?php wc_get_template( 'loop/no-products-found.php' ); ?>
-				<?php endif; ?>
+		<div class="row tags-page-info">
+			<div class="col-xs-12 tags-page-info-col">
+        <span class="tags-page-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque interdum vulputate quam in lobortis. Aliquam erat volutpat. Donec sagittis diam nec lorem eleifend, quis feugiat velit consequat. Nunc tempus, nisi viverra tincidunt accumsan, nisi sapien auctor purus, et sodales sem urna placerat elit.
+        </span>
+				<p class="current-tags">TAGS: <span class="tag-keyword">"<?php single_cat_title(); ?>"</span></p>
 			</div>
 		</div>
-		<!-- /.row -->
+		<div class="row sample tags">
+			<div class="col-xs-6 sample-item">
+				<div class="row category tags">
+					<div class="col-xs-4 category-name">
+						<span>TIN TỨC</span>
+					</div>
+					<div class="col-xs-8 type-list"></div>
+				</div>
+				<div class="news-container">
+					<?php
+					$term_tags = get_queried_object();
+					$args_sp = array(
+						'post_type'=>'news_info',
+						'tax_query'=>array(
+							array(
+								'taxonomy' => 'post_tag',
+								'field'    => 'slug',
+								'terms'    =>$term_tags->slug
+							)
+						)
+					);
+					$tintuc = new WP_Query($args_sp);
+					if($tintuc->have_posts()):
+						?>
+					<?php while($tintuc->have_posts()):$tintuc->the_post(); ?>
+					<article class="row archive">
+
+						<div class="featured-thumb col-xs-12">
+							<a href="<?php the_permalink() ?>">
+								<?php the_post_thumbnail(array(271,169,'bfi_thumb'=>true),array('class' => 'img-responsive')); ?>
+							</a>
+						</div>
+						<div class="col-xs-12 entry-container">
+							<header class="entry-header">
+								<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h1>
+
+								<div class="entry-meta">
+									<span class="posted-on"><?php the_time('d/m/Y'); ?></span>
+								</div>
+								<!-- .entry-meta -->
+							</header>
+							<!-- .entry-header -->
+
+							<div class="entry-content">
+								<p><?php the_excerpt(); ?><a class="more" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">Read More</a></p>
+							</div>
+							<!-- .entry-content -->
+						</div>
+					</article>
+					<?php endwhile; wp_reset_query(); ?>
+					<?php endif; ?>
+				</div>
+			</div>
+			<div class="col-xs-6 sample-item">
+				<div class="row category tags">
+					<div class="col-xs-4 category-name">
+						<span>SẢN PHẨM</span>
+					</div>
+					<div class="col-xs-8 type-list"></div>
+				</div>
+				<?php if ( have_posts() ) : ?>
+					<?php
+					?>
+
+						<?php woocommerce_product_subcategories(); ?>
+
+						<?php while ( have_posts() ) : the_post(); ?>
+							<div class="thumbnail">
+								<a href="<?php the_permalink(); ?>">
+									<?php the_post_thumbnail(array(236,330),array('class'=>'img-responsive main-img')); ?>
+								</a>
+								<div class="caption">
+									<a href="<?php the_permalink(); ?>"><span class="caption-title"><?php hhs_brand_2(); ?></span></a>
+									<hr>
+									<?php
+									$product = new WC_Product(get_the_ID());
+									?>
+									<p class="caption-description"><?php echo get_the_title().$product->get_sku(); ?></p>
+									<div class="caption-price">
+										<?php
+										$price = get_post_meta( get_the_ID(), '_regular_price', true);
+										$sale = get_post_meta( get_the_ID(), '_sale_price', true);
+										?>
+										<div class="price-info">
+											<?php echo '<p class="old-price">'.number_format($price,0,'.','.').' VNĐ</p>'?>
+											<?php echo '<p class="new-price">'.number_format($sale,0,'.','.').' VNĐ</p>'?>
+										</div>
+										<div class="cart">
+											<a href="<?php echo esc_url( $product->add_to_cart_url());?>"><img src="<?php echo get_template_directory_uri()?>/img/product-cart.png"></a>
+										</div>
+									</div>
+								</div>
+							</div>
+
+						<?php endwhile; // end of the loop. ?>
+
+						<?php
+						/**
+						 * woocommerce_after_shop_loop hook
+						 *
+						 * @hooked woocommerce_pagination - 10
+						 */
+						?>
+					<?php
+					do_action( 'woocommerce_after_shop_loop' );
+					?>
+
+				<?php elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) : ?>
+					<?php wc_get_template( 'loop/no-products-found.php' ); ?>
+				<?php endif; ?>
+
+
+			</div>
+		</div>
+
 	</div>
-</div>
-<?php get_template_part('aside','footer'); ?>
 </article>
 <?php get_footer(); ?>
